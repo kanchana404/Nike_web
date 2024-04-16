02607fc595cf15f8457e9dd86d8310e6f1c21cd6
Kavitha Kanchana <146015143+kanchana404@users.noreply.github.com>
2024-04-17 02:34:44 +0530

    SuperQuality_done

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({label, iconURL}) => {
   return (
     <button className='flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red'>
        {label}
-<img src={iconURL} alt="arrowRightIcon" className='ml-2 rounded-full w-5 h-5 ' />
+{iconURL && <img src={iconURL} alt="arrowRightIcon" className='ml-2 rounded-full w-5 h-5 ' />}
     </button>
   )
 }
--- src/Sections/SuperQuality.jsx
+++ src/Sections/SuperQuality.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
+import Button from '../Components/Button'
+import { shoe8 } from '../assets/images'
 
 const SuperQuality = () => {
   return (
-    <div>SuperQuality</div>
-  )
+    <section
+      id='about-us'
+      className='flex justify-between items-center max-lg:flex-col gap-10 w-full max-container'>
+      <div className='flex flex-1 flex-col'>
+
+
+        <h2 className='font-palanquin text-4xl capitalize font-bold'>
+          We Provide You
+          <span className='text-coral-red'> Super</span>
+
+          <br />
+          <span className='text-coral-red'> Quality</span>
+          Shoes
+        </h2>
+        <p className='mt-4 lg:max-w-lg info-text'>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus, cupiditate itaque cum, ab soluta officia repudiandae laborum molestiae consequuntur fuga obcaecati earum maiores repellendus. Id, praesentium sequi! Debitis, perferendis beatae?
+          Quae dolorum laboriosam voluptates sapiente repellendus,
+        </p>
+        <p className='mt-4 lg:max-w-lg info-text'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia repudiandae qui doloremque laboriosam! Nostrum reiciendis quibusdam sunt quas,
+        </p>
+
+        <div className='mt-11 '>
+          <Button label='View Detal' />
+        </div>
+
+      </div>
+      <div className='flex-1 flex justify-center items-center'>
+        <img src={shoe8} alt="shoe8" width={570} height={522}/>
+      </div>
+    </section>)
 }
 
 export default SuperQuality
